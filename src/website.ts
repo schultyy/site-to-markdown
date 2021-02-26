62ece3be3578f5a9041eeed530868c97feafa506
Jan Schulte <jan@work-with-jan.com>
2021-02-26 15:23:45 -0500

    disable eslint complaint

--- src/website.ts
+++ src/website.ts
@@ -1,4 +1,5 @@
 const fetch = require('node-fetch');
+// eslint-disable-next-line @typescript-eslint/naming-convention
 const TurndownService = require('turndown');
 
 export default class Website {
